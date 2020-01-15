4a92b573790c9c8cacbd3944715768c1729dfb5d
Adam <mindreeper2420@gmail.com>
2020-01-15 14:32:31 -0500

    Cleanup build processes

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/eadda60c-6780-4720-869d-ea925937a7e2/deploy-status)](https://app.netlify.com/sites/rhddx/deploys)
+[![Automated Release Notes by gren](https://img.shields.io/badge/%F0%9F%A4%96-release%20notes-00B2EE.svg)](https://github-tools.github.io/github-release-notes/)
 
 # RHDDX Frontend Documentation Site
 
 
 ## Local Development
 
-All processes are controlled through Node/Gulp. As such, clone this repository and run:
+All processes are controlled through Node/Gulp (even though this is a Jekyll JAMstack site). As such, clone this repository and run:
 
 ```bash
 npm install
 gulp development
 ```
-Jekyll will watch your project folder for changes.
+Gulp will watch the project folders for changes.
+```json
+/assets
+/styles
+/pages
+/_data
+/_includes
+/_layouts
+```
 
 Now navigate to [localhost:4000](http://localhost:4000/) to preview the site, and
 [localhost:4000/admin](http://localhost:4000/admin) to log into the CMS.
--- gulpfile.js
+++ gulpfile.js
@@ -23,11 +23,17 @@ const
   imagemin          =  require('gulp-imagemin'),
   sass              =  require('gulp-sass'),
   postcss           =  require('gulp-postcss'),
+  cssnano           =  require('cssnano'),
   header            =  require('gulp-header'),
   pkg               =  require('./package.json'),
   clean             =  require('gulp-clean'),
+  pngquant          =  require('imagemin-pngquant'),
+  mozjpeg           =  require ('imagemin-mozjpeg'),
+  scaleImages       =  require('gulp-scale-images'),
+  flatMap           =  require('flat-map').default,
+  path              =  require('path'),
+  cp                =  require('child_process'),
   sourcemaps        =  devBuild ? require('gulp-sourcemaps') : null,
-  browserSync       =  devBuild ? require('browser-sync') : null,
 
   // Set the banner content
   banner = ['/*!\n',
@@ -51,7 +57,7 @@ const
 
   const cssConfig = {
 
-    dev         : dir.dev + 'styles/**/**/*.scss',
+    dev         : dir.dev + 'styles/**/*.scss',
     watch       : dir.src + 'styles/**/*.scss',
     build       : dir.src + '_site/assets',
 
@@ -76,42 +82,45 @@ const
   //  gulp.watch('./styles/**/*.{scss,css}', buildSass);
   // }
   // Configure the browserSync task
-  gulp.task('browserSync', (done) => {
-    browserSync.init({
-      server: {
-        baseDir: ''
-      },
-      ui: {
-        port: 8001 // customize port for browserSync UI
-      },
-      port: 8080, // use 8080 to prevent conflicts with other localhosts
-      reloadOnRestart: true,
-      notify: false // prevent the browserSync notification from appearing
-    });
+  // gulp.task('browserSync', (done) => {
+  //   browserSync.init({
+  //     server: {
+  //       baseDir: ''
+  //     },
+  //     ui: {
+  //       port: 8001 // customize port for browserSync UI
+  //     },
+  //     port: 8080, // use 8080 to prevent conflicts with other localhosts
+  //     reloadOnRestart: true,
+  //     notify: false // prevent the browserSync notification from appearing
+  //   });
 
-    done();
-  });
+  //   done();
+  // });
 
-  // convert custom scss files to css using PostCSS
-  // @ts-ignore
-  gulp.task('cssProd', (done) => {
+  sass.compiler = require('node-sass');
+
+  //
+  // This function is only to be used by the deployment script,
+  // used by the automated build processes
+  //
+  function buildSass(cb) {
     return gulp.src([
-      '!./styles/rhdp.scss', // ignore the rhdp.scss file - only for automated builds
-      './styles/**/*.scss' // sets director to return as './styles/**/*.scss'
+      '!./styles/rhdp.scss', // ignore the rhdp.scss file due to filepath errors in automated builds
+      './styles/partials/*.scss', // include the partial files for main.scss
+      './styles/main.scss' // include the customized site file
     ])
-    .pipe(sourcemaps ? sourcemaps.init() : noop())
     .pipe(sass(cssConfig.sassOpts).on('error', sass.logError)) // sets the configuration options for building SCSS files
-    .pipe(postcss([ autoprefixer() ]))
+    .pipe(postcss([ autoprefixer(), cssnano() ]))
     .pipe(sourcemaps ? sourcemaps.write() : noop())
     .pipe(size({ showFiles:true }))
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(gulp.dest(cssConfig.build)) // dump compiled SCSS files to './_site/assets/'
-    .pipe(browserSync ? browserSync.reload({ stream: true }) : noop());
-  });
+    .pipe(gulp.dest(cssConfig.build));
+  }
 
   // convert custom scss files to css using PostCSS
   // @ts-ignore
-  gulp.task('cssDev', (done) => {
+  function cssDev(cb){
     return gulp.src(cssConfig.dev) // sets director to return as './styles/**/*.scss'
     .pipe(sourcemaps ? sourcemaps.init() : noop())
     .pipe(sass(cssConfig.sassOpts).on('error', sass.logError)) // sets the configuration options for building SCSS files
@@ -119,26 +128,15 @@ const
     .pipe(sourcemaps ? sourcemaps.write() : noop())
     .pipe(size({ showFiles:true }))
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(gulp.dest(cssConfig.build)) // dump compiled SCSS files to './_site/assets/'
-    .pipe(browserSync ? browserSync.reload({ stream: true }) : noop());
-  });
+    .pipe(gulp.dest(cssConfig.build)); // dump compiled SCSS files to './_site/assets/'
+  }
 
-  // @ts-ignore
-  gulp.task('sass-watch', gulp.series('cssDev'), (done) => {
-    browserSync.reload();
-    done();
-  });
+  function watchSass(cb) {
+    gulp.watch(cssConfig.dev, cssDev);
+  }
 
   // Image resizing using gulp-scale-images and
   // optimization using imagemin with non-default plugins
-
-  // This function makes two variants of each source image
-  const cp = require('child_process');
-  const scaleImages = require('gulp-scale-images');
-  const flatMap = require('flat-map').default;
-  const pngquant = require('imagemin-pngquant');
-  const mozjpeg = require ('imagemin-mozjpeg');
-  const path = require('path');
 
   var exec = require('child_process').exec;
 
@@ -164,21 +162,30 @@ const
 
   // Minimize all of the images located under './assets/img/'
   // Only touches .jpg, .jpeg, .png, and .svg files
-  gulp.task('minimize-images', (done) => {
+  function minimizeImages(cb) {
     return gulp.src('./assets/img/**/*.{jpg,jpeg,png,svg}')
       .pipe(newer('./_site/assets/img/'))
       .pipe(flatMap(retinaVersions))
       .pipe(scaleImages(imageFileName))
       .pipe(imagemin([mozjpeg(), pngquant()]))
       .pipe(gulp.dest('./_site/assets/img/'));
-  });
+  }
 
   // Copy font assets
-  gulp.task('copyFonts', (done) => {
-    gulp.src('./assets/themes/custom/rhdp2/fonts/patternfly/**/*.*').pipe(gulp.dest('./_site/themes/custom/rhdp2/fonts/patternfly/'));
+  function copyFonts(cb) {
+    gulp.src('./assets/themes/custom/rhdp2/fonts/patternfly/**/*.*')
+    .pipe(gulp.dest('./_site/themes/custom/rhdp2/fonts/patternfly/'));
+    cb();
+  }
 
-    done();
-  });
+  // Task to clean _site/ and other Jekyll caches
+  function cleanJekyll(cb) {
+    cp.exec('bundle exec jekyll clean', function(err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  }
 
   // Task to build Jekyll
   // Note that here Bundler is used to manage the Ruby gems
@@ -192,41 +199,49 @@ const
   }
 
   // Task to serve the website locally
-  gulp.task('serve-jekyll', (done) => {
+  function serveJekyll(cb) {
     cp.exec('bundle exec jekyll serve --livereload', function(err, stdout, stderr) {
-      console.log(stdout);
-      console.log(stderr);
-      done(err);
-    });
-  });
-
-  // Install submodule dependencies
-  gulp.task('buildSubmodule', function (cb) {
-    exec('ping localhost', function(err, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
       cb(err);
     });
-    // exec('npm install', function(err, stdout, stderr) {
-    //   console.log(stdout);
-    //   console.log(stderr);
-    //   cb(err);
-    // });
-  });
+  }
 
-  // exports.submodule = buildSubmodule;
+  // ---- WIP ---- //
+  // Install submodule dependencies
+  // ---- WIP ---- //
 
-  // exports.build = gulp.series(
-  //   buildSubmodule
-  // );
+  // gulp.task('buildSubmodule', function (cb) {
+  //   exec('ping localhost', function(err, stdout, stderr) {
+  //     console.log(stdout);
+  //     console.log(stderr);
+  //     cb(err);
+  //   });
+  // });
+  exports.cssDev   =  cssDev;
+  exports.sass     =  buildSass;
+  exports.watch    =  watchSass;
+  exports.jekyll   =  buildJekyll;
+  exports.serve    =  serveJekyll;
+  exports.clean    =  cleanJekyll;
+  exports.imagemin =  minimizeImages;
 
-  // @ts-ignore
-  gulp.task('development', gulp.series('minimize-images', 'cssDev', gulp.parallel('sass-watch', 'serve-jekyll', (done) => {
-    done();
-  })));
+  exports.build = gulp.series(
+    cleanJekyll,
+    gulp.parallel(copyFonts, cssDev, minimizeImages),
+    buildJekyll
+  );
 
-  // @ts-ignore
-  gulp.task('prod', gulp.series('copyFonts', 'minimize-images', 'copyFonts', buildJekyll, (done) => {
-    done();
-  }));
+  exports.development = gulp.series(
+    copyFonts,
+    minimizeImages,
+    gulp.parallel(watchSass, serveJekyll)
+  );
+
+  exports.deploy = gulp.series(
+    copyFonts,
+    minimizeImages,
+    buildSass,
+    buildJekyll
+  );
 })();
