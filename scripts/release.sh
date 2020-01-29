c8ea514afda7d541b77102fee9fa9f38b9c6bd09
Adam <mindreeper2420@gmail.com>
2020-01-29 13:04:13 -0500

    Add Release Scripts

---
+++ scripts/release.sh
@@ -0,0 +1,25 @@
+if [ "`git status -s`" ]
+then
+  echo "You have unstaged commits. Please commit any pending changes."
+  exit 1;
+fi
+
+echo "Checkout Master branch"
+git checkout master
+
+echo "Pull latest from master"
+git pull --rebase origin master
+
+echo "Update NPM Version"
+VERSION=${1?Error: deliniate major, minor, path or prerelease}
+npm version "$VERSION"
+
+echo "Bump Tag Number"
+NAME=${1?Error: no tag given}
+git tag -a $NAME -m "$NAME"
+
+echo "Update Tags/Releases"
+git push origin master --folow-tags
+
+echo "Update Changelog"
+gren release -P --override && gren changelog --override
