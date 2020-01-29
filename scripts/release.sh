da1be205bbcaa0ef9d654e1a0f357f1b8328a0b8
Adam <mindreeper2420@gmail.com>
2020-01-29 13:16:18 -0500

    Simplify scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,12 +14,5 @@ echo "Update NPM Version"
 VERSION=${1?Error: deliniate major, minor, path or prerelease}
 npm version "$VERSION"
 
-echo "Bump Tag Number"
-NAME=${1?Error: no tag given}
-git tag -a $NAME -m "$NAME"
-
-echo "Update Tags/Releases"
-git push origin master --follow-tags
-
 echo "Update Changelog"
 gren release -P --override && gren changelog --override
